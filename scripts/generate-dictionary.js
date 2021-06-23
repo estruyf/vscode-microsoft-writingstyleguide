bead52fb30e1d0988c362f68f5862c44d2151985
Elio Struyf <info@estruyf.be>
2021-06-23 09:04:30 +0200

    Updated links for referenced content

--- scripts/generate-dictionary.js
+++ scripts/generate-dictionary.js
@@ -4,12 +4,54 @@ const path = require('path');
 const matter = require('gray-matter');
 const cheerio = require('cheerio');
 const { gzip } = require('node-gzip');
-const MarkdownIt = require('markdown-it');
-const striptags = require('striptags');
+const md = require('markdown-it');
 
-const md = new MarkdownIt();
+const converter = new md({ html: true, breaks: true });
 
 const searchPath = `**/microsoft-style-guide/styleguide/a-z-word-list-term-collections/**/*.md`;
+const msUrl = "https://docs.microsoft.com/en-us/style-guide";
+const parts = "docs.microsoft.com/en-us/style-guide/a-z-word-list-term-collections/alpha".split('/');
+
+const processLinks = ($, linkElms, content) => {
+
+  const fLinks = linkElms.filter(i => !$(i).attr("href").startsWith(`http`));
+  const uLinks = [...new Set(fLinks)];
+
+  for (const link of uLinks) {
+    const $link = $(link);
+    const fileLink = $link.attr('href');
+    let updatedLink = fileLink;
+
+    if (updatedLink.endsWith(`.md`)) {
+      updatedLink = updatedLink.substring(0, fileLink.length - 3);
+    }
+
+    if (updatedLink.startsWith(`/`)) {
+      updatedLink = `${msUrl}${updatedLink}`;
+    }
+    
+    if (updatedLink.startsWith(`~`)) {
+      updatedLink = `${msUrl}${updatedLink.substring(1)}`;
+    }
+    
+    if (updatedLink.startsWith(`../`)) {
+      var count = (updatedLink.match(/\.\.\//g) || []).length;
+
+      let newParts = Object.assign([], parts);
+      newParts = newParts.slice(0, parts.length - count);
+      
+      updatedLink = `https://${newParts.join("/")}/${updatedLink.replace(/\.\.\//g, "")}`;
+    }
+
+    if (updatedLink) {
+      content = content.replace(`(${fileLink})`, `(${updatedLink})`);
+    }
+  }
+
+  return content;
+};
+
+
 
 (async () => {
   const entries = await fg([searchPath]);
@@ -24,38 +66,24 @@ const searchPath = `**/microsoft-style-guide/styleguide/a-z-word-list-term-colle
     if (fs.existsSync(filePath)) {
       const contents = fs.readFileSync(filePath, { encoding: 'utf-8' });
       const data = matter(contents);
-      const htmlContent = md.render(data.content);
+      const htmlContent = converter.render(data.content);
       const $ = cheerio.load(htmlContent);
 
       const header = $('h1');
+      const anchorElms = $(`a`).toArray();
 
       if (header.length > 0) {
         const words = header.first().text().split(',').map(w => w.toLowerCase().trim());
 
-        // const htmlContent = $.html();
-        // let content = htmlContent.trim();
+        let content = data.content;
 
-        // const lt = new RegExp(`&lt;`, "g");
-        // content = content.replace(lt, "<");
-
-        // const gt = new RegExp(`&gt;`, "g");
-        // content = content.replace(gt, ">");
-
-        // const amp = new RegExp(`&amp;`, "g");
-        // content = content.replace(amp, "&");
-
-        // content = striptags(content);
-
-        // content = content.replace(/\n/g, `\n\n`);
-        // content = content.replace(/\n\n\n\n/g, `\n\n`);
-
-        // while (content.endsWith(`\n`)) {
-        //   content = content.substring(0, content.length - 2);
-        // }
+        if (anchorElms && anchorElms.length > 0) {
+          content = processLinks($, anchorElms, content);
+        }
 
         dictionary.push({
           words,
-          content: data.content
+          content
         });
       }
     }
